33e14a58a5e801f7e94a20786dc4fc0e214c12aa
Luca1152 <t.luca1152@gmail.com>
2021-12-06 14:05:07 +0200

    Add sign up and log in pages
    Now the button in the top right leads somewhere.

---
+++ conectare.php
@@ -0,0 +1,45 @@
+<?php
+    include 'protected/commons.php';
+    include 'protected/database.php';
+
+    connect_database();
+
+    page_header("Lib - Biblioteca ta");
+?>
+
+    <main class="section">
+        <form method="POST" action="/inregistrare-post.php"
+              style="width: 50%; position: relative; margin-left: auto; margin-right: auto">
+            <h1 class="title">Conectare</h1>
+            <div class="field">
+                <label class="label">
+                    E-mail
+                    <div class="control has-icons-left has-icons-right">
+                        <input class="input" type="email" name="email" placeholder="Adresa ta de mail..." value="">
+                    </div>
+                </label>
+            </div>
+
+            <div class="field" style="margin-top: 1.5rem;">
+                <label class="label">Parolă
+                    <div class="control has-icons-left has-icons-right">
+                        <input class="input" type="password" name="password" placeholder="Parola contului..." value="">
+                    </div>
+                </label>
+            </div>
+
+            <div class="field is-grouped"
+                 style="display: flex; align-items: center; justify-content:space-between; 100%; margin-top: 1.5rem;">
+                <div>
+                    Nu ai cont? Înregistrează-te <a href="/inregistrare.php">aici</a>.
+                </div>
+                <div class="control">
+                    <button class="button is-primary is-rounded">Conectare</button>
+                </div>
+            </div>
+        </form>
+    </main>
+
+<?php
+    page_footer();
+?>
---
+++ inregistrare.php
@@ -0,0 +1,73 @@
+<?php
+    include 'protected/commons.php';
+    include 'protected/database.php';
+
+    connect_database();
+
+    page_header("Lib - Biblioteca ta");
+?>
+
+    <main class="section">
+        <form method="POST" action="/inregistrare-post.php"
+              style="width: 50%; position: relative; margin-left: auto; margin-right: auto">
+            <h1 class="title">Înregistrare</h1>
+
+            <div class="field is-grouped" style="gap: 2rem;">
+                <label class="label">
+                    Nume
+                    <div class="control has-icons-left has-icons-right">
+                        <input class="input" type="text" name="first_name" placeholder="Numele tău..." value=""
+                               required>
+                    </div>
+                </label>
+
+                <label class="label">
+                    Prenume
+                    <div class="control has-icons-left has-icons-right">
+                        <input class="input" type="text" name="last_name" placeholder="Prenumele tău..." value=""
+                               required>
+                    </div>
+                </label>
+            </div>
+
+            <div class="field">
+                <label class="label">
+                    E-mail
+                    <div class="control has-icons-left has-icons-right">
+                        <input class="input" type="email" name="email" placeholder="Adresa ta de mail..." value=""
+                               required>
+                    </div>
+                </label>
+            </div>
+
+            <div class="field" style="margin-top: 1.5rem;">
+                <label class="label">Parolă
+                    <div class="control has-icons-left has-icons-right">
+                        <input class="input" type="password" name="password" placeholder="Parola contului..." required>
+                    </div>
+                </label>
+            </div>
+
+            <div class="field" style="margin-top: 1.5rem;">
+                <label class="label">Repetă parola
+                    <div class="control has-icons-left has-icons-right">
+                        <input class="input" type="password" name="password" placeholder="Aceeași parolă..." required>
+                    </div>
+                </label>
+            </div>
+
+            <div class="field is-grouped"
+                 style="display: flex; align-items: center; justify-content:space-between; 100%; margin-top: 1.5rem;">
+                <div>
+                    Ai deja cont? Conectează-te <a href="/conectare.php">aici</a>.
+                </div>
+                <div class="control">
+                    <button class="button is-primary is-rounded">Înregistrare</button>
+                </div>
+            </div>
+        </form>
+    </main>
+
+<?php
+    page_footer();
+?>
