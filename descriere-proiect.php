1caadfa8c7c4681bde6df10a0a7b3777eb44443b
Luca1152 <t.luca1152@gmail.com>
2021-12-06 10:57:46 +0200

    Update project structure and database diagram
    Changed the structure of the files a bit. Also, updated the database diagram from project's description so it's a conceputal diagram (and not a hybrid).

--- descriere-proiect.php
+++ descriere-proiect.php
@@ -1,18 +1,18 @@
 <?php
-    include 'commons.php';
-    include 'database.php';
-
+    include 'protected/database.php';
+    include 'protected/commons.php';
     connect_database();
 
     page_header("Lib - Descriere proiect");
-    echo '
+?>
+
     <main class="section">
         <div class="content">
             <h2 class="subtitle">Descriere proiect</h2>
             <p>Lib este un website de administrare a unei biblioteci.</p>
             <p>
                 Acesta poate fi accesat de <strong>membri</strong> cât și de <strong>administratori</strong>.
-                 "Entitățile" aplicației sunt cărțile. În funcție de tipul de utilizator,
+                "Entitățile" aplicației sunt cărțile. În funcție de tipul de utilizator,
                 se poate interacționa cu aplicația după cum urmează:
             </p>
             <ul>
@@ -28,29 +28,33 @@
                     <ul>
                         <li>Toate abilitățile unui utilizator nelogat</li>
                         <li>Poate vedea numărul de cărți disponibile în bibliotecă pentru un anumit titlu</li>
-                        <li>Poate împrumuta și returna cărți</li>
+                        <li>Poate rezerva cărți (urmând a le ridica fizic)</li>
                     </ul>
                 </li>
                 <li>
-                    <strong>Administrator</strong>
+                    <strong>Bibliotecar</strong>
                     <ul>
                         <li>Toate abilitățile unui utilizator nelogat si unui membru logat</li>
+                        <li>Poate oferi cărți celor ce le-au rezervat online</li>
+                        <li>Poate primi cărți, returnate în urma unui împrumut</li>
                         <li>Poate adăuga, modifica și șterge cărțile din bibliotecă</li>
                         <li>Poate adăuga comentarii cărților individuale (cum ar fi starea lor)</li>
                     </ul>
                 </li>
             </ul>
-            <p>A se nota că se va face distincție între o <strong>carte</strong> și o <strong>copie disponibilă</strong> 
-            a cărții. Astfel că o carte (cu un anumit titlu, scrisă de un anumit autor, etc.) poate avea mai multe copii 
-            în bibliotecă. Acest lucru poate fi util pentru a urmări exact cine are ce carte, cât și a avea anumite
-            comentarii pentru fiecare copie (poate o carte e într-o stare mai proasta; trebuie să știm starea pentru 
-            a putea penaliza membrii ce returnează cărțile într-o stare mai rea).</p>
+            <p>A se nota că se va face distincție între o <strong>carte</strong> și o <strong>copie disponibilă</strong>
+                a cărții. Astfel că o carte (cu un anumit titlu, scrisă de un anumit autor, etc.) poate avea mai multe
+                copii
+                în bibliotecă. Acest lucru poate fi util pentru a urmări exact cine are ce carte, cât și a avea anumite
+                comentarii pentru fiecare copie (poate o carte e într-o stare mai proasta; trebuie să știm starea pentru
+                a putea penaliza membrii ce returnează cărțile într-o stare mai rea).</p>
         </div class="content">
         <div class="content">
-            <h2 class="subtitle">Diagramă entiate-relație</h2>
-            <img src="/images/diagrama-entitate-relatie.jpg" alt="Diagramă entiate relație">
+            <h2 class="subtitle">Diagramă conceptuală</h2>
+            <img src="/images/diagrama-conceptuala.jpg" alt="Diagramă conceptuală">
         </div>
     </main>
-    ';
+
+<?php
     page_footer();
 ?>
--- index.php
+++ index.php
@@ -1,16 +1,18 @@
 <?php
-    include 'commons.php';
-    include 'database.php';
+    include 'protected/commons.php';
+    include 'protected/database.php';
 
     connect_database();
 
     page_header("Lib - Biblioteca ta");
-    echo '
+?>
+
     <main class="section">
         <div class="container">
             Descrierea proiectului se găsește <strong><a href="/descriere-proiect.php">aici</a></strong>.
         </div>
     </main>
-    ';
+
+<?php
     page_footer();
 ?>
