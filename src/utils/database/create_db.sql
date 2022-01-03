a4ba892206458479e8704b1a35350c55ba9acb0f
Luca1152 <t.luca1152@gmail.com>
2022-01-03 12:35:50 +0200

    Change database structure
    Added categories to books, multiple authors, etc.

--- src/utils/database/create_db.sql
+++ src/utils/database/create_db.sql
@@ -32,56 +32,71 @@ CREATE TABLE users
 
 CREATE TABLE authors
 (
-    author_id  INT         NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(64) NOT NULL,
-    last_name  VARCHAR(64) NOT NULL,
-    birth_year INT,
-    death_year INT,
-    PRIMARY KEY (author_id),
-    UNIQUE KEY unique_name (first_name, last_name)
+    author_id INT                NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name      VARCHAR(64) UNIQUE NOT NULL
 );
 
 CREATE TABLE publishers
 (
-    publisher_id INT         NOT NULL AUTO_INCREMENT,
-    name         VARCHAR(64) NOT NULL,
-    PRIMARY KEY (publisher_id),
-    UNIQUE (name)
+    publisher_id INT                NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name         VARCHAR(64) UNIQUE NOT NULL
+);
+
+CREATE TABLE categories
+(
+    category_id INT                NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name        VARCHAR(64) UNIQUE NOT NULL
 );
 
 CREATE TABLE books
 (
-    book_id      INT          NOT NULL AUTO_INCREMENT,
-    title        VARCHAR(128) NOT NULL,
-    edition      VARCHAR(128),
-    author_id    INT          NOT NULL,
-    publisher_id INT          NOT NULL,
-    pages_count  INT,
-    PRIMARY KEY (book_id),
-    FOREIGN KEY (author_id) REFERENCES authors (author_id),
-    FOREIGN KEY (publisher_id) REFERENCES publishers (publisher_id),
-    UNIQUE KEY unique_book (title, author_id, publisher_id)
+    book_id          INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title            VARCHAR(128) NOT NULL,
+    cover_url        VARCHAR(128),
+    publisher_id     INT          NOT NULL,
+    publication_year INT,
+    isbn             INT          NOT NULL,
+    pages_count      INT,
+    FOREIGN KEY (publisher_id) REFERENCES publishers (publisher_id)
 );
 
-CREATE TABLE book_instances
+CREATE TABLE books_authors
 (
-    book_instance_id INT      NOT NULL AUTO_INCREMENT,
-    book_id          INT      NOT NULL,
-    date_added       DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    comments         TEXT,
-    PRIMARY KEY (book_instance_id),
+    book_id   INT NOT NULL,
+    author_id INT NOT NULL,
+    PRIMARY KEY (book_id, author_id),
+    FOREIGN KEY (book_id) REFERENCES books (book_id),
+    FOREIGN KEY (author_id) REFERENCES authors (author_id)
+);
+
+CREATE TABLE books_categories
+(
+    book_id     INT NOT NULL,
+    category_id INT NOT NULL,
+    PRIMARY KEY (book_id, category_id),
+    FOREIGN KEY (book_id) REFERENCES books (book_id),
+    FOREIGN KEY (category_id) REFERENCES categories (category_id)
+);
+
+CREATE TABLE copies
+(
+    copy_id    INT      NOT NULL AUTO_INCREMENT,
+    book_id    INT      NOT NULL,
+    date_added DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    comments   TEXT,
+    PRIMARY KEY (copy_id),
     FOREIGN KEY (book_id) REFERENCES books (book_id)
 );
 
 CREATE TABLE borrows
 (
-    borrow_id        INT NOT NULL AUTO_INCREMENT,
-    user_id          INT NOT NULL,
-    book_instance_id INT NOT NULL,
-    borrow_date      DATE,
-    return_due_date  DATE,
-    return_date      DATE,
+    borrow_id       INT NOT NULL AUTO_INCREMENT,
+    user_id         INT NOT NULL,
+    copy_id         INT NOT NULL,
+    borrow_date     DATE,
+    return_due_date DATE,
+    return_date     DATE,
     PRIMARY KEY (borrow_id),
     FOREIGN KEY (user_id) REFERENCES users (user_id),
-    FOREIGN KEY (book_instance_id) REFERENCES book_instances (book_instance_id)
+    FOREIGN KEY (copy_id) REFERENCES copies (copy_id)
 );
