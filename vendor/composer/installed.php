d5ba923762d43b15b829cbba4c1cb0619f2fc9d7
Luca1152 <t.luca1152@gmail.com>
2022-01-04 19:54:39 +0200

    Add composer
    Added composer for dependency management.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '3acc36ff297118566e910654afb4cb34f1f8c6b1',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '3acc36ff297118566e910654afb4cb34f1f8c6b1',
+            'dev_requirement' => false,
+        ),
+        'amenadiel/jpgraph' => array(
+            'pretty_version' => 'v4.1.1',
+            'version' => '4.1.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../amenadiel/jpgraph',
+            'aliases' => array(),
+            'reference' => 'e196d51a520a4182cc26240b4a0744495706f11a',
+            'dev_requirement' => false,
+        ),
+        'fpdf/fpdf' => array(
+            'pretty_version' => '1.83.2',
+            'version' => '1.83.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../fpdf/fpdf',
+            'aliases' => array(),
+            'reference' => '6aa31c9b70a3aef2a63f79144fec79b62bad4bb0',
+            'dev_requirement' => false,
+        ),
+    ),
+);
