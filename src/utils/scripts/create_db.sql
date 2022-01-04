d226dfd78652e59e3130c23721676ef2a6760dd3
Luca1152 <t.luca1152@gmail.com>
2022-01-04 10:28:44 +0200

    Tweak database structure
    Added description to books, whether an author is the main one (so I know which to display first in case there are more), qualifier (such as "Translator"), and indexes to books categories (so they are in a specific order).

--- src/utils/scripts/create_db.sql
+++ src/utils/scripts/create_db.sql
@@ -1,6 +1,9 @@
 DROP TABLE IF EXISTS borrows;
-DROP TABLE IF EXISTS book_instances;
+DROP TABLE IF EXISTS copies;
+DROP TABLE IF EXISTS books_categories;
+DROP TABLE IF EXISTS books_authors;
 DROP TABLE IF EXISTS books;
+DROP TABLE IF EXISTS categories;
 DROP TABLE IF EXISTS publishers;
 DROP TABLE IF EXISTS authors;
 DROP TABLE IF EXISTS users;
@@ -52,6 +55,7 @@ CREATE TABLE books
 (
     book_id                INT                 NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title                  VARCHAR(128) UNIQUE NOT NULL,
+    description            TEXT,
     cover_url              VARCHAR(256),
     publisher_id           INT                 NOT NULL,
     first_publication_year INT,
@@ -61,8 +65,10 @@ CREATE TABLE books
 
 CREATE TABLE books_authors
 (
-    book_id   INT NOT NULL,
-    author_id INT NOT NULL,
+    book_id        INT  NOT NULL,
+    author_id      INT  NOT NULL,
+    is_main_author BOOL NOT NULL DEFAULT FALSE,
+    qualifier      VARCHAR(64),
     PRIMARY KEY (book_id, author_id),
     FOREIGN KEY (book_id) REFERENCES books (book_id),
     FOREIGN KEY (author_id) REFERENCES authors (author_id)
@@ -72,6 +78,7 @@ CREATE TABLE books_categories
 (
     book_id     INT NOT NULL,
     category_id INT NOT NULL,
+    list_index  INT NOT NULL,
     PRIMARY KEY (book_id, category_id),
     FOREIGN KEY (book_id) REFERENCES books (book_id),
     FOREIGN KEY (category_id) REFERENCES categories (category_id)
@@ -117,35 +124,40 @@ VALUES ('Classics'),
 
 
 -- Pride and Prejudice
-INSERT INTO books(title, cover_url, publisher_id, first_publication_year, pages_count)
+INSERT INTO books(title, description, cover_url, publisher_id, first_publication_year, pages_count)
 VALUES ('Pride and Prejudice',
+        'Since its immediate success in 1813, Pride and Prejudice has remained one of the most popular novels in the English language. Jane Austen called this brilliant work "her own darling child" and its vivacious heroine, Elizabeth Bennet, "as delightful a creature as ever appeared in print." The romantic clash between the opinionated Elizabeth and her proud beau, Mr. Darcy, is a splendid performance of civilized sparring. And Jane Austen''s radiant wit sparkles as her characters dance a delicate quadrille of flirtation and intrigue, making this book the most superb comedy of manners of Regency England.',
         'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1320399351l/1885.jpg', 5, 1813, 279);
 
-INSERT INTO books_authors(book_id, author_id)
-VALUES (5, 5);
+INSERT INTO books_authors(book_id, author_id, is_main_author)
+VALUES (5, 5, TRUE);
 
-INSERT INTO books_categories(book_id, category_id)
-VALUES (5, 5),
-       (5, 15),
-       (5, 25);
+INSERT INTO books_categories(book_id, category_id, list_index)
+VALUES (5, 5, 0),
+       (5, 15, 1),
+       (5, 25, 2);
 
 INSERT INTO copies(book_id)
 VALUES (5);
 
 
 -- Sense and Sensibility
-
-INSERT INTO books(title, cover_url, publisher_id, first_publication_year, pages_count)
+INSERT INTO books(title, description, cover_url, publisher_id, first_publication_year, pages_count)
 VALUES ('Sense and Sensibility',
+        '\'The more I know of the world, the more am I convinced that I shall never see a man whom I can really love. I require so much!\'
+
+Marianne Dashwood wears her heart on her sleeve, and when she falls in love with the dashing but unsuitable John Willoughby she ignores her sister Elinor''s warning that her impulsive behaviour leaves her open to gossip and innuendo. Meanwhile Elinor, always sensitive to social convention, is struggling to conceal her own romantic disappointment, even from those closest to her. Through their parallel experience of love—and its threatened loss—the sisters learn that sense must mix with sensibility if they are to find personal happiness in a society where status and money govern the rules of love.
+
+This edition includes explanatory notes, textual variants between the first and second editions, and Tony Tanner\'s introduction to the original Penguin Classic edition.',
         'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1397245675l/14935._SY475_.jpg', 15, 1811,
         409);
 
-INSERT INTO books_authors(book_id, author_id)
-VALUES (15, 5);
+INSERT INTO books_authors(book_id, author_id, is_main_author)
+VALUES (15, 5, TRUE);
 
-INSERT INTO books_categories(book_id, category_id)
-VALUES (15, 5),
-       (15, 15),
-       (15, 25);
+INSERT INTO books_categories(book_id, category_id, list_index)
+VALUES (15, 5, 0),
+       (15, 15, 1),
+       (15, 25, 2);
 
 COMMIT;
